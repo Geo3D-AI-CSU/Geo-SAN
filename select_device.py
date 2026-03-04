327e1c3c61b4484d4c8502edc31ddcb3242db88e
Zhenxi Fang-CSU <ZhenxiFang@csu.edu.cn>
2026-03-04 10:49:04 +0800

    Update select_device.py

--- select_device.py
+++ select_device.py
@@ -5,33 +5,31 @@ import numpy as np
 def select_device(desired_gpu=2):
     if torch.cuda.is_available():
         num_gpus = torch.cuda.device_count()
-        print(f"可用的 GPU 数量: {num_gpus}")
         for i in range(num_gpus):
             gpu_name = torch.cuda.get_device_name(i)
             print(f"GPU {i}: {gpu_name}")
 
         if desired_gpu < num_gpus:
-            # 使用 torch.cuda.set_device 来设置默认 GPU
+            # Use torch.cuda.set_device to set the default GPU.
             torch.cuda.set_device(desired_gpu)
-            # 返回设备对象
+            # Return the device object
             device = torch.device(f'cuda:{desired_gpu}')
-            print(f"使用设备: {device}")
+            print(f"Use of equipment: {device}")
         else:
-            print(f"GPU {desired_gpu} 不可用。使用 CPU。")
             device = torch.device('cpu')
     else:
-        print("CUDA 不可用。使用 CPU。")
+        print("CUDA is unavailable. Using CPU.")
         device = torch.device('cpu')
 
     return device
 
 def set_random_seed(seed=42):
     """
-    设置随机种子以确保结果的可重复性。
+    Set a random seed to ensure the reproducibility of results.
     """
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  # 如果使用多个GPU
+    torch.cuda.manual_seed_all(seed)  # If multiple GPUs are used
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
